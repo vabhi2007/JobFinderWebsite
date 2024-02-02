c533b181f3b9a5f27c313aa11392c1d78c07ad7d
SacchinS <sacchinsar30@gmail.com>
2024-02-02 10:52:20 -0800

    added stuff to home

--- web-app/src/components/pages/home.jsx
+++ web-app/src/components/pages/home.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import "./home.css"
-import plantBackground from "./images/plant background trimmed.png"
+import React from 'react';
+import "./home.css";
+import plantBackground from "./images/plant background trimmed.png";
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="center">
       <div className='imageBox'>
-        <img className = "img" src={plantBackground}></img>
+        <img className="img" src={plantBackground} alt="Plant Background" />
+        <div className="textOverlay">
+          <h1>Welcome to <br /> Verdantia!</h1>
+          <p className="caption">Where Sustainability meets Innovation.</p>
+          <button className="pillButton">Join the team</button>
+        </div>
       </div>
-   
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
