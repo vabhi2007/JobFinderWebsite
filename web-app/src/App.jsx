230ddcd7a291813c589c0257491294e53bd1fe01
vabhi2007 <70085064+vabhi2007@users.noreply.github.com>
2024-02-01 23:48:54 -0800

    added green bar and navbar in progress

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -5,11 +5,12 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/pages/home'
 import Jobs from './components/pages/Jobs'
 import Benefits from './components/pages/Benefits'
-
+import Bar from './components/Navbar/Bar'
 function App() {
 
   return (
     <>
+      <Bar/>
       <Navbar/>
       <Routes>
         <Route path='/' element = {<Home/>}/>
