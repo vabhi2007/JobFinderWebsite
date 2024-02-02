9fb0135315fd8bfd19bb24bf86bce3cedb45adb2
vabhi2007 <70085064+vabhi2007@users.noreply.github.com>
2024-02-02 00:15:06 -0800

    everything messed up

--- web-app/src/components/pages/Jobs.jsx
+++ web-app/src/components/pages/Jobs.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import "./Jobs.css"
-import plantBackground from "./images/plant background.png"
+import plantBackground from "./images/plantbackground.png"
 const Jobs = () => {
   return (
-    <>
-    <div className='landing'>
-    <img src={plantBackground} alt="" />
-      <div className='title'>Careers</div>
+    <div>
+      
+      <div className='imageBox'>
+        <img className = "img" src={plantBackground}></img>
+      </div>
+      
+      
     </div>
-     </>
   )
 }
 
