1a7cb77708b097e56b8bfc1b1ca61bdf5286ce7b
Aakarsh Balla <aakarshballa@gmail.com>
2024-02-02 13:09:44 -0800

    Added new font and title to Benefits

--- web-app/src/components/pages/Benefits.jsx
+++ web-app/src/components/pages/Benefits.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import "./home.css"
+import "./Benefits.css"
 import plantBackground from "./images/plant background trimmed.png"
 
 const Benefits = () => {
   return (
+    
     <div>
-      <header className='header'>
-        <h1>Employee Benefits</h1>
-      </header>
+ 
       <div className='imageBox'>
+      <header className='header'>
+          <h1>Career</h1>
+          <h1>Benefits</h1>
+        </header>
         <img className = "img" src={plantBackground}></img>
       </div>
 
     </div>
+
   )
 }
 
