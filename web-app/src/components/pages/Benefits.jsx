9ec99c34cdadf49f705301e51c6420f2f09f1b20
Aakarsh Balla <aakarshballa@gmail.com>
2024-02-02 00:07:02 -0800

    Made changes to benefits pages

--- web-app/src/components/pages/Benefits.jsx
+++ web-app/src/components/pages/Benefits.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import "./Benefits.css"
+import plantBackground from "./images/plant background.png"
 const Benefits = () => {
   return (
-    <div>
-      Benefits
+    <>
+    <div className='benefits'>
+        <img src={plantBackground} alt="" />
+      <div className='title'>Benefits</div>
+      <h3>Career Benefits</h3>
     </div>
+   </>
+    
   )
 }
 
