6098d5cc2a847051926bfc3e2e51b3b927eb424a
Aakarsh Balla <aakarshballa@gmail.com>
2024-02-02 22:30:56 -0800

    added function to this branch

--- web-app/src/components/pages/Benefits.jsx
+++ web-app/src/components/pages/Benefits.jsx
@@ -3,6 +3,12 @@ import "./home.css"
 import plantBackground from "./images/plant background trimmed.png"
 
 const Benefits = () => {
+  const handleSeeMoreClick = () => {
+    const targetElement = document.getElementById('scrollToSection'); // Adjust the ID to your target section
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
   return (
     <div>
     <div>
